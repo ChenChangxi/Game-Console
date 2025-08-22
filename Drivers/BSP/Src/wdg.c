4f62d3430fe331ddee6ec1b677aebbf83f651293
ChenChangxi <ccx1016875161@gmail.com>
2025-08-22 15:21:43 +0800

    修复了H743中IWDG窗口模式的BUG

--- Drivers/BSP/Src/wdg.c
+++ Drivers/BSP/Src/wdg.c
@@ -20,6 +20,7 @@ void iwdg_init(uint8_t div, uint16_t load) {
     iwdg_handler.Instance       = IWDG1;
     iwdg_handler.Init.Prescaler = div;
     iwdg_handler.Init.Reload    = load;
+    iwdg_handler.Init.Window    = IWDG_WINDOW_DISABLE;   /* H743的IWDG有窗口模式 */
     HAL_IWDG_Init(&iwdg_handler);
 }
 
@@ -31,7 +32,7 @@ void wwdg_init(uint8_t div, uint8_t cou, uint8_t win) {
     wwdg_handler.Init.Prescaler = div;
     wwdg_handler.Init.Counter   = cou;
     wwdg_handler.Init.Window    = win;
-    wwdg_handler.Init.EWIMode   = WWDG_EWI_ENABLE;   /* 唤醒中断 */
+    wwdg_handler.Init.EWIMode   = WWDG_EWI_ENABLE;       /* 唤醒中断 */
     HAL_WWDG_Init(&wwdg_handler);
 }
 
